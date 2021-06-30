30140cd29568ca32449ca890113a9747a5d68b1e
abraham saldivar <saldivara1010@gmail.com>
2021-06-30 18:56:21 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,40 +1,94 @@
 // Code your solution in this file!
 
-/*
-const arrayToCut = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const n = 5 //get the first 5 items
-const newArray = arrayToCut.slice(0, n) 
-*/
-const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 
-function returnFirstTwoDrivers(drivers){
-
-drivers = drivers.slice (0, 2)
-   
-return drivers
+function returnFirstTwoDrivers(drivers) {
+    drivers = drivers.slice(0,2)
+    return drivers
 }
-function returnLastTwoDrivers(drivers){
 
-drivers = drivers.slice(-2)
-    
-return drivers
+
+function returnLastTwoDrivers(drivers){
+    drivers = drivers.slice(-2)
+    return drivers
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
-function createFareMultiplier(integer){
 
-    return totalFare => totalFare * integer
-    
-}
-function fareDoubler(fare){
-    return (fare) * 2;
-
-}
-function fareTripler(fare){
-    return fare * 3;
+function createFareMultiplier(num){
+    return totalFare => totalFare * num
 }
 
-function selectDifferentDrivers( firstNamesArray ,driverFunction){
-    return driverFunction(firstNamesArray) 
+function fareDoubler(createFareMultiplier){
+        return createFareMultiplier * 2 
 }
+
+function fareTripler(createFareMultiplier){
+    return createFareMultiplier * 3
+}
+
+function selectDifferentDrivers(drivers, callBack){
+    return callBack(drivers)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
